58511e209e4bef7d7c5660f8bad4059d053a9914
jamesivarson <jamesivarson@gmail.com>
2025-04-07 09:02:25 -0400

    B: sending to github

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill((0, 0, 0))
         pygame.display.flip()
     print("Starting Asteroids!")
